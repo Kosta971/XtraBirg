995b2336f47e7e181d5695a309789bd9ea6c1e48
Kosta971 <rgsjcsn@gmail.com>
2025-07-10 14:40:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -170,5 +170,6 @@ LOGIN = """<form method="post">
 
 # Запуск
 if __name__ == '__main__':
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     socketio.run(app, host="0.0.0.0", port=5000)
