04c18a7703e741006e9a618338c0a099746fa6c7
Kosta971 <rgsjcsn@gmail.com>
2025-07-10 14:45:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -172,4 +172,4 @@ LOGIN = """<form method="post">
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
